0770cc1c5ba25eb2474af9e59d27f36c6ce23e4f
Santiago <santymiguel311@gmail.com>
2026-07-01 11:24:49 -0300

    se optimizo la carga de personajes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,13 @@
 let Personajes = [];
+let contenedor = document.querySelector("#contenedor");
+const buscar = document.querySelector("#btnBuscar");
+/* funcion de carga de personajes*/
 async function cargarPersonajes() {
-  const respuesta = await fetch("https://thesimpsonsapi.com/api/characters");
-  const datos = await respuesta.json();
-  Personajes = datos.results;
-  console.log(Personajes);
+  try {
+    const respuesta = await fetch("https://thesimpsonsapi.com/api/characters");
+    const datos = await respuesta.json();
+    Personajes = datos.results;
+  } catch {
+    console.error("error al cargar el listado");
+  }
 }
